fce1d0cb3ce586e4984758370913e701948f97c1
wnsxor1993 <https://wnsxor1993@github.com>
2023-07-14 15:29:41 +0900

    chore: 프로젝트 생성

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Zeto on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
