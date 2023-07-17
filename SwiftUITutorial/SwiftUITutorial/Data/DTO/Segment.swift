5a6abbce82a453004b44d382dccb239d39a70273
wnsxor1993 <https://wnsxor1993@github.com>
2023-07-17 17:27:24 +0900

    feat: FourthStep_ShapesManager 구현

---
+++ SwiftUITutorial/SwiftUITutorial/Data/DTO/Segment.swift
@@ -0,0 +1,15 @@
+//
+//  Segment.swift
+//  SwiftUITutorial
+//
+//  Created by Zeto on 2023/07/17.
+//
+
+import Foundation
+
+struct Segment {
+    
+    let line: CGPoint
+    let curve: CGPoint
+    let control: CGPoint
+}
---
+++ SwiftUITutorial/SwiftUITutorial/Domain/Manager/ShapesManager.swift
@@ -0,0 +1,77 @@
+//
+//  ShapesManager.swift
+//  SwiftUITutorial
+//
+//  Created by Zeto on 2023/07/17.
+//
+
+import CoreGraphics
+
+class ShapesManager {
+    
+    enum Shape {
+        case hexagon
+    }
+    
+    private let shape: Shape
+    
+    private var segments: [Segment] = []
+    private var adjustment: CGFloat = 0
+    
+    init(shape: Shape) {
+        self.shape = shape
+        
+        switch shape {
+        case .hexagon:
+            self.configureHexagon()
+        }
+    }
+}
+
+// MARK: Hexagon
+private extension ShapesManager {
+    
+    func configureHexagon() {
+        self.configureHexagonAdjustment()
+        self.configureHexagonSegments()
+    }
+    
+    func configureHexagonAdjustment() {
+        self.adjustment = 0.085
+    }
+    
+    func configureHexagonSegments() {
+        self.segments = [
+            Segment(
+                line: CGPoint(x: 0.60, y: 0.05),
+                curve: CGPoint(x: 0.40, y: 0.05),
+                control: CGPoint(x: 0.50, y: 0.00)
+            ),
+            Segment(
+                line: CGPoint(x: 0.05, y: 0.20 + adjustment),
+                curve: CGPoint(x: 0.00, y: 0.30 + adjustment),
+                control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+            ),
+            Segment(
+                line: CGPoint(x: 0.00, y: 0.70 - adjustment),
+                curve: CGPoint(x: 0.05, y: 0.80 - adjustment),
+                control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+            ),
+            Segment(
+                line: CGPoint(x: 0.40, y: 0.95),
+                curve: CGPoint(x: 0.60, y: 0.95),
+                control: CGPoint(x: 0.50, y: 1.00)
+            ),
+            Segment(
+                line: CGPoint(x: 0.95, y: 0.80 - adjustment),
+                curve: CGPoint(x: 1.00, y: 0.70 - adjustment),
+                control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+            ),
+            Segment(
+                line: CGPoint(x: 1.00, y: 0.30 + adjustment),
+                curve: CGPoint(x: 0.95, y: 0.20 + adjustment),
+                control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+            )
+        ]
+    }
+}
